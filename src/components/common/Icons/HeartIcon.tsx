1f8347a419c47c862adc9d33ba857b2f9f96f88d
yuni <hys990730@gmail.com>
2024-07-31 01:05:24 +0900

    refactor: HeartIcon 컴포넌트에 SVGR 적용
    - SVG 내용을 별도의 .svg 파일로 추출
    - SVGR을 사용하여 SVG에서 React 컴포넌트 생성
    - HeartIcon을 SVGR로 생성된 컴포넌트를 사용하도록 수정

--- src/components/common/Icons/HeartIcon.tsx
+++ src/components/common/Icons/HeartIcon.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { ReactComponent as HeartSvg } from './heartIcon.svg';
+
 interface HeartIconProps {
   fill?: string;
   stroke?: string;
@@ -13,18 +15,5 @@ export const HeartIcon: React.FC<HeartIconProps> = ({
   width = 24,
   height = 24,
 }) => {
-  return (
-    <svg
-      width={width}
-      height={height}
-      viewBox="0 0 24 24"
-      fill={fill}
-      stroke={stroke}
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
-    </svg>
-  );
+  return <HeartSvg width={width} height={height} fill={fill} stroke={stroke} />;
 };
