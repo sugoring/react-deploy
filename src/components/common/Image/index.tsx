66bd7c259a34910696cc0613396bddbde268f2de
yuni <hys990730@gmail.com>
2024-08-02 17:57:05 +0900

    fix: use placeholder image when data is missing in Image componen

--- src/components/common/Image/index.tsx
+++ src/components/common/Image/index.tsx
@@ -3,9 +3,19 @@ import styled from '@emotion/styled';
 type Props = {
   radius?: 'circle' | number;
   ratio?: 'square' | 'auto' | number;
+  src?: string;
 } & React.ImgHTMLAttributes<HTMLImageElement>;
 
-export const Image = ({ ...props }: Props) => <Wrapper {...props} />;
+const placeholderImage =
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLTbwEGi00FUChLrIEPiavJSDhyLqUIG-1mg&s';
+
+export const Image = ({ src, ...props }: Props) => {
+  const handleError = (event: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    event.currentTarget.src = placeholderImage;
+  };
+
+  return <Wrapper src={src || placeholderImage} onError={handleError} {...props} />;
+};
 
 const Wrapper = styled.img<Pick<Props, 'ratio' | 'radius'>>(
   {
